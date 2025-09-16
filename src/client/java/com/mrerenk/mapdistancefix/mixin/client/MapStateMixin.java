cde374da47309acca6b249e306863f3c01aaf652
MrErenK <akbaseren4751@gmail.com>
2025-09-16 15:58:09 +0300

    refactor: Always show the player and add support for servers
    * Also drop version support below 1.19.3, modrinth stats show that they
      are not even downloaded 10 times.

--- src/client/java/com/mrerenk/mapdistancefix/mixin/client/MapStateMixin.java
+++ src/client/java/com/mrerenk/mapdistancefix/mixin/client/MapStateMixin.java
@@ -6,13 +6,38 @@ import java.util.List;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.map.MapIcon;
 import net.minecraft.item.map.MapState;
+import net.minecraft.registry.RegistryKey;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MapState.class)
 public class MapStateMixin {
+
+    @Shadow
+    @Final
+    @Mutable
+    private boolean unlimitedTracking;
+
+    // Force unlimitedTracking = true on construction
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void enableUnlimitedTracking(
+        int centerX,
+        int centerZ,
+        byte scale,
+        boolean showIcons,
+        boolean unlimitedTracking,
+        boolean locked,
+        RegistryKey<?> dimension,
+        CallbackInfo ci
+    ) {
+        this.unlimitedTracking = true;
+    }
 
     @Inject(method = "getIcons", at = @At("RETURN"), cancellable = true)
     private void convertOffMapPlayerIcons(
