25542267b5409c78dc0203f830a51d67ec2f22e4
MrErenK <akbaseren4751@gmail.com>
2025-11-27 14:57:01 +0300

    fix: Arrow spinning by itself in the nether * Thanks to https://github.com/Syrikal/Nether-Map-Port/blob/main/src/main/java/com/syric/betternethermap/mixin/MixinMapItemSavedData.java

--- src/main/java/com/mrerenk/mapdistancefix/mixin/MapStateMixin.java
+++ src/main/java/com/mrerenk/mapdistancefix/mixin/MapStateMixin.java
@@ -2,12 +2,14 @@ package com.mrerenk.mapdistancefix.mixin;
 
 import net.minecraft.item.map.MapState;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MapState.class)
@@ -17,6 +19,10 @@ public class MapStateMixin {
     @Final
     @Mutable
     private boolean unlimitedTracking;
+
+    @Shadow
+    @Final
+    public RegistryKey<World> dimension;
 
     // Force unlimitedTracking = true on construction (server-side)
     @Inject(method = "<init>", at = @At("RETURN"))
@@ -31,5 +37,21 @@ public class MapStateMixin {
         CallbackInfo ci
     ) {
         this.unlimitedTracking = true;
+    }
+
+    // Disables the spinning indicator in the Nether by making the game think we're in the Overworld
+    @Redirect(
+        method = "addIcon",
+        at = @At(
+            value = "FIELD",
+            target = "Lnet/minecraft/item/map/MapState;dimension:Lnet/minecraft/registry/RegistryKey;"
+        )
+    )
+    private RegistryKey<World> redirectDimensionCheck(MapState instance) {
+        // When in the Nether, return OVERWORLD to prevent the spinning indicator
+        if (this.dimension == World.NETHER) {
+            return World.OVERWORLD;
+        }
+        return this.dimension;
     }
 }
