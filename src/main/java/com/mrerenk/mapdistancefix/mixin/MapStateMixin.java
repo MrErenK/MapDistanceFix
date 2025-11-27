2359291960fb2ac2105d8ad670fb19a2d5689cbe
MrErenK <akbaseren4751@gmail.com>
2025-11-27 14:40:57 +0300

    fix: Arrow spinning by itself in the nether * Thanks to https://github.com/Syrikal/Nether-Map-Port/blob/main/src/main/java/com/syric/betternethermap/mixin/MixinMapItemSavedData.java

--- src/main/java/com/mrerenk/mapdistancefix/mixin/MapStateMixin.java
+++ src/main/java/com/mrerenk/mapdistancefix/mixin/MapStateMixin.java
@@ -1,13 +1,17 @@
 package com.mrerenk.mapdistancefix.mixin;
 
+import java.util.Map;
+import net.minecraft.item.map.MapDecoration;
 import net.minecraft.item.map.MapState;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MapState.class)
@@ -17,6 +21,14 @@ public class MapStateMixin {
     @Final
     @Mutable
     private boolean unlimitedTracking;
+
+    @Shadow
+    @Final
+    Map<String, MapDecoration> decorations;
+
+    @Shadow
+    @Final
+    public RegistryKey<World> dimension;
 
     // Force unlimitedTracking = true on construction
     @Inject(method = "<init>", at = @At("RETURN"))
@@ -31,5 +43,21 @@ public class MapStateMixin {
         CallbackInfo ci
     ) {
         this.unlimitedTracking = true;
+    }
+
+    // Disables the spinning indicator in the Nether by making the game think we're in the Overworld
+    @Redirect(
+        method = "addDecoration",
+        at = @At(
+            value = "FIELD",
+            target = "Lnet/minecraft/item/map/MapState;dimension:Lnet/minecraft/registry/RegistryKey;"
+        )
+    )
+    private RegistryKey<World> redirectDimensionCheck(MapState instance) {
+        // Always return OVERWORLD to prevent the spinning indicator in Nether
+        if (this.dimension == World.NETHER) {
+            return World.OVERWORLD;
+        }
+        return this.dimension;
     }
 }
