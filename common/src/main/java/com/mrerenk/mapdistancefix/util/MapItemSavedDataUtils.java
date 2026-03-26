ca76cd03a50e1c3a5a9c09ed6f7f0d89d5ff4199
MrErenK <akbaseren4751@gmail.com>
2026-03-26 21:35:10 +0300

    fix: Miscalculation of rotation in nether

--- common/src/main/java/com/mrerenk/mapdistancefix/util/MapItemSavedDataUtils.java
+++ common/src/main/java/com/mrerenk/mapdistancefix/util/MapItemSavedDataUtils.java
@@ -72,17 +72,8 @@ public final class MapItemSavedDataUtils {
         double rotation,
         ResourceKey<Level> dimension
     ) {
-        if (dimension == Level.NETHER) {
-            if (world != null) {
-                int i = (int) (world.getLevelData().getGameTime() / 10L);
-                return (byte) (((i * i * 34187121 + i * 121) >> 15) & 15);
-            } else {
-                return 0;
-            }
-        } else {
-            double d = rotation < 0.0 ? rotation - 8.0 : rotation + 8.0;
-            return (byte) ((int) ((d * 16.0) / 360.0));
-        }
+        double d = rotation < 0.0 ? rotation - 8.0 : rotation + 8.0;
+        return (byte) ((int) ((d * 16.0) / 360.0));
     }
 
     /**
