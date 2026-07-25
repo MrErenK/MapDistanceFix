a71177ecdc6dd14fb2bc699e4da1121a18130642
MrErenK <akbaseren4751@gmail.com>
2026-07-25 19:36:18 +0300

    fix: NeoForge server initialization

--- neoforge/src/main/java/com/mrerenk/mapdistancefix/MapdistancefixNeoForgeServer.java
+++ neoforge/src/main/java/com/mrerenk/mapdistancefix/MapdistancefixNeoForgeServer.java
@@ -1,35 +1,27 @@
 package com.mrerenk.mapdistancefix;
 
 import com.mrerenk.mapdistancefix.command.MapDistanceFixCommand;
-import com.mrerenk.mapdistancefix.config.ModConfigScreen;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Mod(value = "mapdistancefix", dist = Dist.CLIENT)
-public class MapdistancefixNeoForge {
+@Mod(value = "mapdistancefix", dist = Dist.DEDICATED_SERVER)
+public class MapdistancefixNeoForgeServer {
 
     public static final String MOD_ID = "mapdistancefix";
     public static final String MOD_NAME = "MapDistanceFix";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
-    public MapdistancefixNeoForge(
+    public MapdistancefixNeoForgeServer(
         IEventBus modEventBus,
         ModContainer modContainer
     ) {
-        LOGGER.info("Initializing {} (NeoForge)", MOD_NAME);
-
-        // Register the Cloth Config screen to NeoForge's mod list GUI
-        modContainer.registerExtensionPoint(
-            IConfigScreenFactory.class,
-            (client, parent) -> ModConfigScreen.create(parent)
-        );
+        LOGGER.info("Initializing {} (NeoForge Dedicated Server)", MOD_NAME);
 
         NeoForge.EVENT_BUS.addListener(this::onRegisterCommands);
 
